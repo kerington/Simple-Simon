fc44d1573021d969e25f44c05b1ed56e3113b0eb
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-02 15:39:55 -0600

    Adding changes to Simple Simon Game. 	- Centered color boxes in CSS, added margin space, added shadow. 	- ALSO rearranged/organized HTML so that all links to jQuery, Bootstrap, and custom code is formatted correctly (-was previously loading in incorrect order)

---
+++ public/JS/simpleSimonGame.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    "use strict";
+
+$("#boxRed").click(function(){
+	$("#boxRed").fadeOut(300);
+	$("#boxRed").fadeIn(300);
+	console.log("meow");
+
+})
+
+$("#boxYellow").click(function(){
+	$("#boxYellow").fadeOut(300);
+	$("#boxYellow").fadeIn(300);
+	console.log("meow");
+
+})
+
+$("#boxBlue").click(function(){
+	$("#boxBlue").fadeOut(300);
+	$("#boxBlue").fadeIn(300);
+	console.log("meow");
+
+})
+
+$("#boxGreen").click(function(){
+	$("#boxGreen").fadeOut(300);
+	$("#boxGreen").fadeIn(300);
+	console.log("meow");
+
+})
+});
+
