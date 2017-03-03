48add4c77852d5303fa14e2bd368ce410565139d
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-03 14:12:26 -0600

    Updated/Changed CSS look of Simple Simon Game: 	- turned boxes into half moons, added a large border, and added a Point Counter and Start Button.

--- public/JS/simpleSimonGame.js
+++ public/JS/simpleSimonGame.js
@@ -1,79 +1,43 @@
 $(document).ready(function(){
     "use strict";
 
-//     function newRound() {
-//     	var sequence = [1,2,1]; //red, yellow, red
-//     	animate(sequence);
-//     }
-
-//     function animate (sequence) {
-//     	var i = 0;
-//     	var interval = setInterval (function() {
-//     		lightup(sequence[i]);
-
-//     			i++;
-//     			if (i >= sequence.length) {
-//     				clearInterval(interval);
-//     			}
-//     	}, 600);
-//     }
-
-//     function lightup(title) {
-//     	var $tile = $('[data-tile=' + tile + '}').addClass
-//     	window.setTimeout(function() {
-//     		$tile.removeClass('lit');
-//     	}, 300);
-//     }
 
 
-$("#boxRed").click(function(){
-	$("#boxRed").fadeOut(200);
-	$("#boxRed").fadeIn(200);
-	console.log("Red");
-})
-
-$("#boxYellow").click(function(){
-	$("#boxYellow").fadeOut(200);
-	$("#boxYellow").fadeIn(200);
-	console.log("Yellow");
-})
-
-$("#boxBlue").click(function(){
-	$("#boxBlue").fadeOut(200);
-	$("#boxBlue").fadeIn(200);
-	console.log("Blue");
-})
-
-$("#boxGreen").click(function(){
-	$("#boxGreen").fadeOut(200);
-	$("#boxGreen").fadeIn(200);
-	console.log("Green");
-})
+var green = colorButton(1, "green");
+var red = colorButton(2, "red");
+var yellow = colorButton(3, "yellow");
+var blue = colorButton(4, "blue");
 
 
 
-function getRandomColor () {
-	var squares = ["#boxRed", "#boxYellow", "#boxBlue", "#boxGreen"]
-	var randomNumber = Math.floor(Math.random() * squares.length);
-	return squares[randomNumber];
-	console.log("working!");
-}
-
-function animateRandomSquare  () {
-	var squareToAnimate = getRandomColor();
-	animate(squareToAnimate);
-}
 
 
-function getRandomNumberBetween0And(input) {
-	return Math.floor(Math.random() * input)
-}
 
-var squares = ["boxRed", "boxYellow", "boxBlue", "boxGreen"];
 
-var randomIndex = getRandomNumberBetween0And(squares.length);
 
-var randomSquare = squares[randomIndex];
+
+// function getRandomColor () {
+//     var squares = ["#boxRed", "#boxYellow", "#boxBlue", "#boxGreen"]
+//     var randomNumber = Math.floor(Math.random() * squares.length);
+//     return squares[randomNumber];
+//     console.log("working!");
+// }
+
+// function animateRandomSquare  () {
+//     var squareToAnimate = getRandomColor();
+//     animate(squareToAnimate);
+// }
+
+
+// function getRandomNumberBetween0And(input) {
+//     return Math.floor(Math.random() * input)
+// }
+
+// var squares = ["boxRed", "boxYellow", "boxBlue", "boxGreen"];
+
+// var randomIndex = getRandomNumberBetween0And(squares.length);
+
+// var randomSquare = squares[randomIndex];
 
 console.log("meow");
 
