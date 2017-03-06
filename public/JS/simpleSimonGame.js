03c93adc481a269b785664ee07785685301bff19
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-06 11:02:39 -0600

    Adding Simple Simon Game JS and Notes (after watching Niagara presentation)

--- public/JS/simpleSimonGame.js
+++ public/JS/simpleSimonGame.js
@@ -1,71 +1,70 @@
-$(document).ready(function(){
-    "use strict";
+// $(document).ready(function(){
+	"use strict";
 
 var memoryArray = []
 
 
 function newMemory() {
-  var temp = Math.floor((Math.random() * 4) + 1);
-  switch(temp) {
-    case 1:
-      memoryArray.push("#buttonGreen");
-      break;
-    case 2:
-      memoryArray.push("#buttonRed");
-      break;
-    case 3:
-      memoryArray.push("#buttonYellow");
-      break;
-    case 4:
-      memoryArray.push("#buttonBlue");
-      break;
-  }
+	var temp = Math.floor((Math.random() * 4) + 1);
+		switch(temp) {
+		case 1:
+			memoryArray.push("#buttonGreen");
+			break;
+		case 2:
+			memoryArray.push("#buttonRed");
+			break;
+		case 3:
+			memoryArray.push("#buttonYellow");
+			break;
+		case 4:
+			memoryArray.push("#buttonBlue");
+			break;
+	}
 
-  var i = 0;
-  var intervalId = setInterval(function(){
-    animateRandomSquare(memoryArray[i]);
-    if(i < memoryArray.length){
-        i++;
-    }else{
-        clearInterval(intervalId);
-    }
-  },850)
-}
-newMemory();
+	var i = 0;
+	var intervalId = setInterval(function(){
+		animateRandomSquare(memoryArray[i]);
+		if(i < memoryArray.length){
+			i++;
+		}else{
+			clearInterval(intervalId);
+		}
+	},850)
+	}
 
 function animateRandomSquare (buttonId) {
-    $(buttonId) .animate({
-        opacity: 1
-    }, 400, function(){
-        $(buttonId).animate({
-            opacity: .5
-        }, 400);
-    })
+	$(buttonId) .animate({
+		opacity: 1
+	}, 400, function(){
+		$(buttonId).animate({
+			opacity: .5
+		}, 400);
+	})
 };
 
 var clickCount = 0;
 
 $(".fourButtons").click(function(){
-    var clickedId = "#" + $(this).attr("id");
-    console.log("Clicked: " + clickedId);
-    console.log("Array: " + memoryArray[clickCount]);
-    if (clickedId == memoryArray[clickCount]){
-        if (clickCount == memoryArray.length -1 ) {
-            newMemory();
-            clickCount = 0;
-        } else {
-            clickCount++;
-        }
-    }else{
-        memoryArray =[];
-        console.log("u loser!!!");
-    }
+	var clickedId = "#" + $(this).attr("id");
+	console.log("Clicked: " + clickedId);
+	console.log("Array: " + memoryArray[clickCount]);
+	if (clickedId == memoryArray[clickCount]){
+		if (clickCount == memoryArray.length -1 ) {
+			newMemory();
+			clickCount = 0;
+		} else {
+			clickCount++;
+		}
+	}else{
+		memoryArray =[];
+		console.log("u loser!!!");
+	}
 
 })
 
-$("#startButton").click(animateRandomSquare){
-
-}
+$("#startButton").click(function(){
+	newMemory();
+});
 
 
 
@@ -84,5 +83,5 @@ $("#startButton").click(animateRandomSquare){
 // console.log("meow");
 
 
-});
+// });
 
